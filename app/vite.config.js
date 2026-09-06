d3dcf468c4144f188098f50ec75748d9752acc28
Graham Horwood <gruevyhat@gmail.com>
2026-09-05 20:47:05 -0400

    Fix GitHub Pages base path

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'singlefile' ? './' : '/outgunned/',
+  base: mode === 'singlefile' ? './' : '/outgunned-toolkit/',
   plugins: [react(), ...(mode === 'singlefile' ? [viteSingleFile()] : [])],
   build: { target: 'es2020' }
 }))
